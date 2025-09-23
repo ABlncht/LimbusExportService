5c744029b0f480fad109251bca0d6fe229f98fb1
ABlncht <alexandre.blncht@gmail.com>
2025-09-23 11:08:30 +0200

    Update installeur.bat
    oubli \

--- bin/installeur.bat
+++ bin/installeur.bat
@@ -1,6 +1,7 @@
 xcopy /s /y "C:\Users\utilisateur\source\repos\LimbusExportService\bin\Release\*" "C:\ProgramData\LimbusExportService\"
 cd C:\Windows\Microsoft.NET\Framework64\v4.0.30319
-InstallUtil.exe "C:\ProgramDataLimbusExportService\LimbusExportService.exe"
+InstallUtil.exe "C:\ProgramData\LimbusExportService\LimbusExportService.exe"
 @echo off
 echo Installation OK, appuyez sur une touche pour continuer…
 pause > nul
+
